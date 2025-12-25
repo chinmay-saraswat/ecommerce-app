bb670b1a99320f85ba1f96a7d3b2d75d2d6f6ea0
chinmay saraswat <226301072@gkv.ac.in>
2025-12-25 18:02:03 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+import express from "express"
+import path from "path";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest,functions } from "./config/inngest.js";
+
+const app = express();
+app.use(express.json());
+
+const __dirname = path.resolve();
+
+app.use(clerkMiddleware())
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/api",(req,res)=>{
+    res.status(200).json({message:"success"})
+})
+
+// make our app ready for deployment 
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../admin/dist")));
+
+    app.get("/{*any}",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../admin","dist","index.html"));
+    })
+}
+
+const startServer = async()=>{
+    await connectDB();
+    app.listen(ENV.PORT,()=>{console.log("server is running on port 3000 ");        
+    });
+}
+
+startServer();
